0b925fbf31e0c898edaa348ed73480b703495866
HikkiiGayya <mcpe951753@gmail.com>
2023-12-12 12:34:41 -0500

    Ya funcinona la url de imagenes

--- shoes/settings/base.py
+++ shoes/settings/base.py
@@ -50,12 +50,12 @@ THIRD_APPS = [
 INSTALLED_APPS = BASE_APPS + LOCAL_APPS + THIRD_APPS
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ],
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
-    ),
+    # 'DEFAULT_AUTHENTICATION_CLASSES': [
+    #     'rest_framework_simplejwt.authentication.JWTAuthentication',
+    # ],
+    # 'DEFAULT_PERMISSION_CLASSES': (
+    #     'rest_framework.permissions.IsAuthenticated',
+    # ),
     'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema'
 }
 
--- shoes/urls.py
+++ shoes/urls.py
@@ -2,6 +2,7 @@
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf import settings
+from django.conf.urls.static import static
 from django.views.static import serve
 from apps.usuarios.api.views.views import Login, Logout
 from rest_framework_simplejwt.views import (
@@ -22,3 +23,6 @@ urlpatterns = [
     path('pqrs/', include('apps.pqrs.api.routers')),
     path('carrito/', include('apps.carrito.api.routers')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
