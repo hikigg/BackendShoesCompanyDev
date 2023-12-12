35796cc2a1dae57b4167ee8d041c8583d970acb7
HikkiiGayya <mcpe951753@gmail.com>
2023-12-11 19:18:21 -0500

    Arreglo de mostrar nombre

--- apps/carrito/api/serializers/carritodetalle_serializer.py
+++ apps/carrito/api/serializers/carritodetalle_serializer.py
@@ -34,20 +34,20 @@ class CarritoDetalleSerializer(serializers.ModelSerializer):
         carrito_detalle = CarritoDetalle.objects.create(**validated_data)
         return carrito_detalle
 
-    # def to_representation(self, instance):
-    #     return {
-    #         'id': instance.id,
-    #         'cantidad': instance.cantidad,
-    #         'precio': instance.precio,
-    #         'impuesto': instance.impuesto,
-    #         'total': instance.total,
-    #         'producto': instance.producto_id.nombre if instance.producto_id is not None else '',
-    #         'producto_id': instance.producto_id.id if instance.producto_id is not None else '',
-    #         'nombre_usuario': instance.usuario_id.name if instance.usuario_id is not None else '',
-    #         'apellidos_usuario': instance.usuario_id.last_name if instance.usuario_id is not None else '',
-    #         'direccion_usuario': instance.usuario_id.direccion if instance.usuario_id is not None else '',
-    #         'tipo_documento': instance.usuario_id.tipo_documento if instance.usuario_id is not None else '',
-    #         'documento': instance.usuario_id.documento if instance.usuario_id is not None else '',
-    #         'telefono_usuario': instance.usuario_id.telefono if instance.usuario_id is not None else '',
-    #         'cupon': instance.cupones_id.codigo if instance.cupones_id is not None else '',
-    #     }
+    def to_representation(self, instance):
+        return {
+            'id': instance.id,
+            'cantidad': instance.cantidad,
+            'precio': instance.precio,
+            'impuesto': instance.impuesto,
+            'total': instance.total,
+            'producto': instance.producto_id.nombre if instance.producto_id is not None else '',
+            'producto_id': instance.producto_id.id if instance.producto_id is not None else '',
+            'nombre_usuario': instance.usuario_id.name if instance.usuario_id is not None else '',
+            'apellidos_usuario': instance.usuario_id.last_name if instance.usuario_id is not None else '',
+            'direccion_usuario': instance.usuario_id.direccion if instance.usuario_id is not None else '',
+            'tipo_documento': instance.usuario_id.tipo_documento if instance.usuario_id is not None else '',
+            'documento': instance.usuario_id.documento if instance.usuario_id is not None else '',
+            'telefono_usuario': instance.usuario_id.telefono if instance.usuario_id is not None else '',
+            'cupon': instance.cupones_id.codigo if instance.cupones_id is not None else '',
+        }
