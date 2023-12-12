ea8829e6255af379e68dc3efa131d5f2bbd41c57
HikkiiGayya <mcpe951753@gmail.com>
2023-12-12 12:46:21 -0500

    Imagen funciona

--- apps/carrito/models.py
+++ apps/carrito/models.py
@@ -12,7 +12,7 @@ class CarritoDetalle(BaseModel):
     total = models.TextField('Total de articulos', max_length=12, blank=False, null=True)
     producto_id = models.ForeignKey(Producto, on_delete=models.CASCADE, verbose_name='Oferta de articulos', null=True)
     usuario_id = models.ForeignKey(Usuario, on_delete=models.CASCADE, verbose_name='Datos del usuario para los articulos', null=True)
-    cupones_id = models.ForeignKey(CuponesProducto, on_delete=models.CASCADE, verbose_name='Cupon de articulos', null=True)
+    cupones_id = models.ForeignKey(CuponesProducto, on_delete=models.CASCADE, verbose_name='Cupon de articulos', null=False)
     historical = HistoricalRecords(inherit=True)
 
     @property
